4ff36e9f5033c8aaebf6966b0bff9d317e6f170d
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-05-22 20:16:02 -0700

    Add sizes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 import Image from "next/image"
-import { AssetData, AssetKind, assets } from "./assets"
+import { Asset, AssetKind, AssetSize, assets } from "./assets"
 import { useState } from "react"
 
 export default function Home() {
   let [kind, setKind] = useState<AssetKind | undefined>(undefined)
-  let filteredAssets = assets.filter((asset) => kind === undefined || asset.kind === kind)
+  let filteredAssets = assets.filter(
+    (asset) => kind === undefined || asset.kind === kind)
+  filteredAssets = filteredAssets.filter(
+    asset => !asset.name.endsWith('.HEIC')
+  )
   return (
     <main>
       <Navigation onKindSelected={setKind} kind={kind} />
@@ -29,15 +33,15 @@ function Navigation({
       <NavigationLink title="Drawing"
         onClick={() => onKindSelected('drawing')}
         selected={kind === 'drawing'}
-      />,
+      />,&nbsp;
       <NavigationLink title="Illustration"
         onClick={() => onKindSelected('illustration')}
         selected={kind === 'illustration'}
-      />,
+      />,&nbsp;
       <NavigationLink title="Painting"
         onClick={() => onKindSelected('painting')}
         selected={kind === 'painting'}
-      />,
+      />,&nbsp;
       <NavigationLink title="Poster"
         onClick={() => onKindSelected('poster')}
         selected={kind === 'poster'}
@@ -61,10 +65,10 @@ function NavigationLink({
 }
 
 function Gallery({ assets }: {
-  assets: AssetData[],
+  assets: Asset[],
 }) {
   return (
-    <div className="flex flex-row flex-wrap">
+    <div className="columns-4">
       {assets.map((asset) => (
         <Tile key={asset.name} asset={asset} />
       ))}
@@ -72,22 +76,34 @@ function Gallery({ assets }: {
   )
 }
 
-function assetSrc(asset: AssetData) {
+function assetSrc(asset: Asset) {
   return `/${asset.name}`
 }
-function assetAlt(asset: AssetData) {
+function assetAlt(asset: Asset) {
   return `${asset.title} (${asset.year})`
+}
+function assetWidth(asset: Asset) {
+  return asset.size ? parseInt(asset.size.split('x')[0]) : 300
+}
+function assetHeight(asset: Asset) {
+  return asset.size ? parseInt(asset.size.split('x')[1]) : 300
+}
+function assetDescription(asset: Asset) {
+  return `${asset.title} (${asset.year}), ${asset.material}`
 }
 
 function Tile({ asset }: {
-  asset: AssetData,
+  asset: Asset,
 }) {
-  return <div className="w-1/4">
+  return <div className="flex flex-col break-inside-avoid-column">
     <Image
       src={assetSrc(asset)}
       alt={assetAlt(asset)}
-      width={asset.width ?? 300}
-      height={asset.height ?? 300}
+      width={assetWidth(asset)}
+      height={assetHeight(asset)}
     />
+    <span className="text-xs text-lime-500">
+      {assetDescription(asset)}
+    </span>
   </div>
 }
