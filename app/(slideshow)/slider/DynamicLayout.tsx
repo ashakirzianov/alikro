83e569cd2be8467d03875d778032de803c8d1709
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-07-13 10:22:57 -0700

    Extract dynamic layout

---
+++ app/(slideshow)/slider/DynamicLayout.tsx
@@ -0,0 +1,110 @@
+import {
+    Asset, assetAlt, assetHeight, assetSrc, assetWidth,
+} from "@/shared/assets"
+import Image from "next/image"
+
+export function DynamicLayout({
+    slides, aspect, fractions, scroll,
+}: {
+    slides: Asset[][],
+    aspect: number,
+    fractions: readonly [number, number, number],
+    scroll: number,
+}) {
+    let [one, two, three] = fractions
+    let lines = computeLines({
+        slides,
+        aspect,
+        fractions,
+    })
+    return <>
+        <AssetLine
+            assets={lines[0]}
+            scroll={scroll}
+            height={`${one}dvh`}
+        />
+        <AssetLine
+            assets={lines[1].reverse()}
+            scroll={scroll}
+            height={`${two}dvh`}
+            direction="right"
+        />
+        <AssetLine
+            assets={lines[2]}
+            scroll={scroll}
+            height={`${three}dvh`}
+        />
+    </>
+}
+
+function AssetLine({ assets, scroll, height, direction }: {
+    assets: Asset[],
+    scroll: number,
+    height: string,
+    direction?: 'left' | 'right',
+}) {
+    return <div style={{
+        overflow: 'hidden',
+        width: '100vw',
+    }}>
+        <div className="flex flex-row flex-nowrap" style={{
+            justifyContent: direction === 'right' ? 'flex-end' : 'flex-start',
+            height,
+            position: 'relative',
+            left: direction === 'right' ? scroll : -scroll,
+        }}>
+            {assets.map((asset, i) =>
+                <Image
+                    key={i}
+                    src={assetSrc(asset)}
+                    alt={assetAlt(asset)}
+                    width={assetWidth(asset)}
+                    height={assetHeight(asset)}
+                    style={{
+                        maxHeight: '100%',
+                        width: 'auto',
+                    }}
+                />
+            )}
+        </div>
+    </div>
+}
+
+function computeLines({ slides, aspect, fractions }: {
+    slides: Asset[][],
+    aspect: number,
+    fractions: readonly number[],
+}) {
+    let lines: Asset[][] = []
+    let totalFraction = fractions.reduce((a, b) => a + b, 0)
+    let aspects = fractions.map(fraction => totalFraction * aspect / fraction)
+    for (let a of aspects) {
+        let { line, remaining } = computeLine({ slides, aspect: a })
+        lines.push(line)
+        slides = remaining
+    }
+    return lines
+}
+
+function computeLine({ slides, aspect }: {
+    slides: Asset[][],
+    aspect: number,
+}) {
+    let line: Asset[] = []
+    let remaining: Asset[][] = []
+    let width = 0
+    for (let slide of slides) {
+        let reversed = [...slide].reverse()
+        width += aspect
+        while (width > 0) {
+            let asset = reversed.pop()
+            if (asset === undefined)
+                break
+            let assetAspect = assetWidth(asset) / assetHeight(asset)
+            line.push(asset)
+            width -= assetAspect
+        }
+        remaining.push(reversed.reverse())
+    }
+    return { line, remaining }
+}
--- app/(slideshow)/slider/page.tsx
+++ app/(slideshow)/slider/page.tsx
@@ -1,29 +1,12 @@
 'use client'
 import { useEffect, useState } from "react"
-import Image from "next/image"
-import {
-    Asset, assetAlt, assetHeight, assetSrc, assetWidth, assets, assetsForTags,
-} from "@/shared/assets"
+import { assets, assetsForTags } from "@/shared/assets"
 import { Slider } from "./Slider"
+import { DynamicLayout } from "./DynamicLayout"
 
 export default function Page() {
     let [scroll, setScroll] = useState(0)
     let aspect = useAspectRatio()
-    let fractions = [30, 50, 20]
-    let [one, two, three] = fractions
-    let lines = computeLines({
-        slides: [
-            assetsForTags(assets, 'drawing'),
-            assetsForTags(assets, 'illustration'),
-            assetsForTags(assets, 'painting'),
-            assetsForTags(assets, 'poster'),
-            assetsForTags(assets, 'collage'),
-            assetsForTags(assets, 'tattoo'),
-        ],
-        aspect,
-        fractions,
-    })
-    let adjustedScroll = scroll * aspect
     return <div>
         <div className="flex flex-col" style={{
             position: 'fixed',
@@ -31,21 +14,18 @@ export default function Page() {
             height: '100vh',
             zIndex: -1,
         }}>
-            <AssetLine
-                assets={lines[0]}
-                scroll={adjustedScroll}
-                height={`${one}dvh`}
-            />
-            <AssetLine
-                assets={lines[1].reverse()}
-                scroll={adjustedScroll}
-                height={`${two}dvh`}
-                direction="right"
-            />
-            <AssetLine
-                assets={lines[2]}
-                scroll={adjustedScroll}
-                height={`${three}dvh`}
+            <DynamicLayout
+                slides={[
+                    assetsForTags(assets, 'drawing'),
+                    assetsForTags(assets, 'illustration'),
+                    assetsForTags(assets, 'painting'),
+                    assetsForTags(assets, 'poster'),
+                    assetsForTags(assets, 'collage'),
+                    assetsForTags(assets, 'tattoo'),
+                ]}
+                aspect={aspect}
+                fractions={[30, 50, 20]}
+                scroll={scroll * aspect}
             />
         </div>
         <Slider onScroll={setScroll}>
@@ -74,78 +54,6 @@ function TextSlide({
         alignSelf: 'flex-start',
         width: 'auto',
     }}>{text}</div>
-}
-
-function AssetLine({ assets, scroll, height, direction }: {
-    assets: Asset[],
-    scroll: number,
-    height: string,
-    direction?: 'left' | 'right',
-}) {
-    return <div style={{
-        overflow: 'hidden',
-        width: '100vw',
-    }}>
-        <div className="flex flex-row flex-nowrap" style={{
-            justifyContent: direction === 'right' ? 'flex-end' : 'flex-start',
-            height,
-            position: 'relative',
-            left: direction === 'right' ? scroll : -scroll,
-        }}>
-            {assets.map((asset, i) =>
-                <Image
-                    key={i}
-                    src={assetSrc(asset)}
-                    alt={assetAlt(asset)}
-                    width={assetWidth(asset)}
-                    height={assetHeight(asset)}
-                    style={{
-                        maxHeight: '100%',
-                        width: 'auto',
-                    }}
-                />
-            )}
-        </div>
-    </div>
-}
-
-function computeLines({ slides, aspect, fractions }: {
-    slides: Asset[][],
-    aspect: number,
-    fractions: number[],
-}) {
-    let lines: Asset[][] = []
-    let totalFraction = fractions.reduce((a, b) => a + b, 0)
-    let aspects = fractions.map(fraction => totalFraction * aspect / fraction)
-    for (let a of aspects) {
-        let { line, remaining } = computeLine({ slides, aspect: a })
-        lines.push(line)
-        slides = remaining
-    }
-    return lines
-}
-
-function computeLine({ slides, aspect }: {
-    slides: Asset[][],
-    aspect: number,
-}) {
-    let line: Asset[] = []
-    let remaining: Asset[][] = []
-    let width = 0
-    for (let slide of slides) {
-        let reversed = [...slide].reverse()
-        width += aspect
-        while (width > 0) {
-            let asset = reversed.pop()
-            if (asset === undefined)
-                break
-            let assetAspect = assetWidth(asset) / assetHeight(asset)
-            line.push(asset)
-            width -= assetAspect
-        }
-        remaining.push(reversed.reverse())
-    }
-    return { line, remaining }
 }
 
 function useAspectRatio() {
