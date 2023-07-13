425f68f6f5872f0f52e2f7eaf7b3bbb3ce954636
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-07-12 22:19:49 -0700

    Cool slideshow

--- app/(slideshow)/slider/page.tsx
+++ app/(slideshow)/slider/page.tsx
@@ -1,20 +1,180 @@
+'use client'
 import { SocialLinks } from "@/shared/SocialLinks"
 import { Asset, assetAlt, assetHeight, assetSrc, assetWidth, assets, assetsForKind, assetsForTags } from "@/shared/assets"
 import { unique } from "@/shared/utils"
 import Image from "next/image"
-import React, { CSSProperties } from "react"
+import React, { CSSProperties, useEffect, useState } from "react"
 
 export default function Page() {
-    return <Slider>
-        <MainSlide />
-        <DrawingsSlide />
-        <IllustrationsSlide />
-        <PaintingsSlide />
-        <PostersSlide />
-        <CollagesSlide />
-        <TattoosSlide />
-    </Slider>
+    let [scroll, setScroll] = React.useState(0)
+    let aspect = useAspectRatio()
+    let fractions = [30, 50, 20]
+    let [one, two, three] = fractions
+    let lines = computeLines({
+        slides: [
+            assetsForTags(assets, 'drawing'),
+            assetsForTags(assets, 'illustration'),
+            assetsForTags(assets, 'painting'),
+            assetsForTags(assets, 'poster'),
+            assetsForTags(assets, 'collage'),
+            assetsForTags(assets, 'tattoo'),
+        ],
+        aspect,
+        fractions,
+    })
+    const textStyle: CSSProperties = {
+        display: 'inline',
+        fontSize: '10vw',
+        // WebkitTextStroke: '2px white',
+        // background: 'linear-gradient(to bottom, rgba(255,255,255,0) 0%, rgba(255,255,255,0.6) 80%, rgba(255,255,255,0) 100%)',
+        background: 'hsl(0, 80%, 80%, 0.6)',
+        alignSelf: 'flex-start',
+        width: 'auto',
+    }
+    let adjustedScroll = scroll * aspect
+    return <div>
+        <div className="flex flex-col" style={{
+            position: 'fixed',
+            width: '100vw',
+            height: '100vh',
+            zIndex: -1,
+        }}>
+            <AssetLine
+                assets={lines[0]}
+                scroll={adjustedScroll}
+                height={`${one}dvh`}
+            />
+            <AssetLine
+                assets={lines[1].reverse()}
+                scroll={adjustedScroll}
+                height={`${two}dvh`}
+                direction="right"
+            />
+            <AssetLine
+                assets={lines[2]}
+                scroll={adjustedScroll}
+                height={`${three}dvh`}
+            />
+        </div>
+        <Slider onScroll={setScroll}>
+            <TextSlide text="Drawings." hue={0} />
+            <TextSlide text="Illustrations." hue={60} />
+            <TextSlide text="Paintings." hue={120} />
+            <TextSlide text="Posters." hue={240} />
+            <TextSlide text="Collages." hue={180} />
+            <TextSlide text="Tattoos." hue={300} />
+        </Slider>
+    </div>
 }
+
+function TextSlide({
+    text, hue,
+}: {
+    text: string,
+    hue: number,
+}) {
+    return <div style={{
+        display: 'inline',
+        fontSize: '10vw',
+        // background: `hsl(${hue}, 100%, 50%, 1)`,
+        background: `hsl(0, 100%, 50%, 1)`,
+        color: 'white',
+        alignSelf: 'flex-start',
+        width: 'auto',
+    }}>{text}</div>
+}
+
+function AssetLine({ assets, scroll, height, direction }: {
+    assets: Asset[],
+    scroll: number,
+    height: string,
+    direction?: 'left' | 'right',
+}) {
+    return <div style={{
+        overflow: 'hidden',
+        width: '100vw',
+    }}>
+        <div className="flex flex-row flex-nowrap" style={{
+            justifyContent: direction === 'right' ? 'flex-end' : 'flex-start',
+            height,
+            position: 'relative',
+            left: direction === 'right' ? scroll : -scroll,
+        }}>
+            {assets.map((asset, i) =>
+                <Image
+                    key={i}
+                    src={assetSrc(asset)}
+                    alt={assetAlt(asset)}
+                    width={assetWidth(asset)}
+                    height={assetHeight(asset)}
+                    style={{
+                        maxHeight: '100%',
+                        width: 'auto',
+                    }}
+                />
+            )}
+        </div>
+    </div>
+}
+
+function computeLines({ slides, aspect, fractions }: {
+    slides: Asset[][],
+    aspect: number,
+    fractions: number[],
+}) {
+    let lines: Asset[][] = []
+    let totalFraction = fractions.reduce((a, b) => a + b, 0)
+    let aspects = fractions.map(fraction => totalFraction * aspect / fraction)
+    for (let a of aspects) {
+        let { line, remaining } = computeLine({ slides, aspect: a })
+        lines.push(line)
+        slides = remaining
+    }
+    return lines
+}
+
+function computeLine({ slides, aspect }: {
+    slides: Asset[][],
+    aspect: number,
+}) {
+    let line: Asset[] = []
+    let remaining: Asset[][] = []
+    let width = 0
+    for (let slide of slides) {
+        console.log(`slide: ${slide[0].kind}, count: ${slide.length}`)
+        let reversed = [...slide].reverse()
+        width += aspect
+        while (width > 0) {
+            let asset = reversed.pop()
+            if (asset === undefined)
+                break
+            let assetAspect = assetWidth(asset) / assetHeight(asset)
+            line.push(asset)
+            width -= assetAspect
+        }
+        remaining.push(reversed.reverse())
+    }
+    return { line, remaining }
+}
+
+function useAspectRatio() {
+    const [aspectRatio, setAspectRatio] = useState(window.innerWidth / window.innerHeight)
+
+    useEffect(() => {
+        function handleResize() {
+            setAspectRatio(window.innerWidth / window.innerHeight)
+        };
+
+        window.addEventListener('resize', handleResize)
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, []) // Empty dependency array means this effect runs once on mount and clean up on unmount
+
+    return aspectRatio
+};
 
 function MainSlide() {
     let background = 'hsl(331 82% 58% / 0.8)'
@@ -116,8 +276,9 @@ function TattoosSlide() {
     />
 }
 
-function Slider({ children }: {
+function Slider({ children, onScroll }: {
     children: React.ReactNode[],
+    onScroll?: (value: number) => void,
 }) {
     return <div style={{
         scrollSnapType: 'y mandatory',
@@ -126,7 +287,13 @@ function Slider({ children }: {
         overflowX: 'hidden',
         overflowY: 'auto',
         scrollPaddingTop: '0vh',
-    }}>
+    }}
+        onScroll={e => {
+            if (onScroll) {
+                onScroll(e.currentTarget.scrollTop)
+            }
+        }}
+    >
         {React.Children.map(children, (child, idx) =>
             <section key={idx} style={{
                 scrollSnapAlign: 'start',
