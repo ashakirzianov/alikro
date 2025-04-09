fbaf51f7f614c81febef377a98a3b47c00de8422
Anton Shakirzianov <ashakirzianov@icloud.com>
2025-04-09 17:20:29 -0300

    Put back authentication

--- app/console/page.tsx
+++ app/console/page.tsx
@@ -1,10 +1,14 @@
 import { getAllAssetMetadata } from "@/shared/metadataStore"
 import ConsolePage from "./ConsolePage"
 import { sortAssets } from "@/shared/assets"
+import { Authenticator, isAuthenticated } from "./Authenticator"
 
 export default async function Page({ searchParams }: {
     searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }) {
+    if (!isAuthenticated()) {
+        return <Authenticator />
+    }
     const resolved = await searchParams
     const unsorted = await getAllAssetMetadata()
     const assets = sortAssets(unsorted)
