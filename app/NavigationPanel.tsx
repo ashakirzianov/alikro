130ec949fc859b4a39eb2fbb6f457e972e7a60c9
Anton Shakirzianov <ashakirzianov@icloud.com>
2025-04-10 20:31:36 -0300

    Show tag in navigation panel

--- app/NavigationPanel.tsx
+++ app/NavigationPanel.tsx
@@ -2,10 +2,11 @@
 import { NavigationLink } from "@/shared/Atoms"
 import { allCollections } from "@/shared/collection"
 import { hrefForCollection } from "@/shared/href"
-import { useSelectedLayoutSegment } from 'next/navigation'
+import { useSelectedLayoutSegments } from 'next/navigation'
 
 export function NavigationPanel() {
-    const segment = useSelectedLayoutSegment()
+    const [first, second] = useSelectedLayoutSegments()
+    const showExtra = second !== undefined && ['tag'].includes(first)
     return (
         <nav className="flex flex-row flex-wrap text-accent text-2xl sm:text-5xl whitespace-nowrap pb-2">
             <NavigationLink
@@ -19,15 +20,25 @@ export function NavigationPanel() {
                         key={collection.id}
                         href={hrefForCollection({ collectionId: collection.id })}
                         title={collection.id}
-                        selected={segment === collection.id}
+                        selected={first === collection.id}
                     />
                 ))
             }
             <NavigationLink title="about"
                 href="/about"
-                selected={segment === 'about'}
+                selected={first === 'about'}
                 last
             />
+            {showExtra && <>
+                <span>{'//'}&nbsp;</span>
+                <NavigationLink
+                    href={`/${first}/${second}`}
+                    title={second}
+                    selected={true}
+                    last={true}
+                />
+            </>
+            }
         </nav>
     )
 }
