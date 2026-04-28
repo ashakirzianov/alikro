9257baa39b9c9c9829d7ab76b50d9e3c0dbadce2
Anton Shakirzianov <ashakirzianov@icloud.com>
2026-04-28 20:54:45 -0300

    Remove unused experimental setting

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: {
-            bodySizeLimit: '10mb',
-        },
-    },
     cacheComponents: true,
     images: {
         minimumCacheTTL: 2678400, // 31 days
